fc03e0f824c1910972838f506060258ac90f2500
xm94630 <xm94630@126.com>
2017-01-21 21:04:00 +0800

    增加jquery的引用。添加canvas的几个使用，已经注释了。还要研究下别的

--- src/index.js
+++ src/index.js
@@ -1,9 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, Link, browserHistory } from 'react-router'
+import $ from 'jquery'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+var l = console.log;
+
+
+
+
+
+
+
+
+
+/*$.ajax({
+  method: "POST",
+  url: "http://10.149.0.84/offer/getOffer",
+  dataType: "json",
+  data: {"idNum": '110110110110110110'}
+}).done(function(data){
+    l(data);
+});*/
+
+
+/*render((
+  <div>offer</div>
+), document.getElementById('root'))
+
+
+
+var mousePressed = false;
+var lastX, lastY;
+var ctx;
+ 
+function InitThis() {
+    ctx = document.getElementById('myCanvas').getContext("2d");
+ 
+    $('#myCanvas').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+ 
+    $('#myCanvas').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+ 
+    $('#myCanvas').mouseup(function (e) {
+        mousePressed = false;
+    });
+        $('#myCanvas').mouseleave(function (e) {
+        mousePressed = false;
+    });
+}
+ 
+function Draw(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 20;
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
+     
+function clearArea() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}         
+
+$(function(){
+	InitThis();
+})   */                    
+
+
+
+
+
+
+
+
