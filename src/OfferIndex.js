0e4ceb4c7027f3e26b1d2b6cff026ef688e33543
xm94630 <xm94630@126.com>
2017-01-22 22:38:38 +0800

    完成offer列表页面

--- src/OfferIndex.js
+++ src/OfferIndex.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import pic1 from './pic1.png'
+import pic1 from './img/pic1.png'
 
 
 export default React.createClass({
   render() {
     return (
-    	<div className="OfferIndex">
+    	<div className="page OfferIndex">
     	    <img src={pic1} width="100%"/>
     	    <p className="MT10">输入身份证号码<br/>快速查询您的offer信息</p>
     	    <div className="sanjiao MT10"></div>
--- src/OfferLists.js
+++ src/OfferLists.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import pic1 from './img/pic2.png'
+import card from './img/card.png'
 
 export default React.createClass({
   render() {
     return (
-    	<div>OfferLists</div>
+    	<div className="page OfferLists">
+    		<div className="picBox">
+	    		<img src={pic1} width="100%"/>
+	    		<span className="studentName">令狐冲</span>
+    		</div>
+    	    <p className="MT10">您有以下录取信息</p>
+    	    <div className="cardGroup">
+	    	    <div className="cardBox MT5">
+	    	    	<img src={card} width="100%"/>
+	    	    	<div className="offerBox">
+	    	    		<div className="offerBoxCon">
+		    	    		录取学院：<span className="schoolName">斯特拉斯克莱德</span><br/>
+							录取专业：<span className="schoolName">艺术与设计</span>
+						</div>
+	    	    	</div>
+	    	    </div>
+        	    <div className="cardBox MT5">
+        	    	<img src={card} width="100%"/>
+        	    	<div className="offerBox">
+        	    		<div className="offerBoxCon">
+    	    	    		录取学院：<span className="schoolName">斯特拉斯克莱德</span><br/>
+    						录取专业：<span className="schoolName">艺术与设计</span>
+    					</div>
+        	    	</div>
+        	    </div>
+        	    
+    	    </div>
+    	    <p className="mini MT2 MB15">指导老师：<span className="teacherName">岳不群</span></p>
+    	</div>
     )
   }
 })
