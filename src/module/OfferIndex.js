fa8018a67131a2b2fb1bc28536fd17c24c6e7136
xm94630 <xm94630@126.com>
2017-01-23 16:00:54 +0800

    测试几个接口

--- src/module/OfferIndex.js
+++ src/module/OfferIndex.js
@@ -4,19 +4,11 @@ import $ from 'jquery'
 var l = console.log;
 
 export default React.createClass({
+
+
   render() {
 
-
-    /*$.ajax({
-      method: "GET",
-      url: "http://os2017.51qiantu.com/offer/getOffer",
-      dataType: "jsonp",
-      jsonp: "callback",
-      data: {"idNum": '140427198902272915'}
-    }).done(function(data){
-        l(data);
-    });*/
-
+    //查询所有offer
     $.ajax({
         type: "GET",
         url: "http://os2017.51qiantu.com/offer/getOffer",
@@ -24,6 +16,51 @@ export default React.createClass({
         jsonp:"callback",
         data: {"idNum": '140427198902272915'},
         success: function(data){
+            console.log(data);
+        }
+    });
+
+    //查询单独offer
+    /*$.ajax({
+        type: "GET",
+        url: "http://os2017.51qiantu.com/offer/detail/"+2,
+        dataType:"jsonp",
+        jsonp:"callback",
+        success: function(data){
+            console.log('==>');
+            console.log(data);
+        }
+    });*/
+
+    //提交offer修改信息
+    /*$.ajax({
+        type: "POST",
+        url: "http://os2017.51qiantu.com/offer/generate",
+        dataType:"json",
+        data: {
+          "reName": '孙悟空',
+          "pic":'jdafoehiwohgfwewterwt',
+          "watermark":1,
+        },
+        success: function(data){
+            console.log(data);
+        }
+    });*/
+
+    //提交学生信息
+    $.ajax({
+        type: "POST",
+        url: "http://os2017.51qiantu.com/student/save",
+        dataType:"json",
+        data: {
+          "name": "徐明",
+          "mobile": "13800001111",
+          "country": "中国",
+          "region": "浙江",
+          "degree": "博士"
+        },
+        success: function(data){
+            console.log('==>');
             console.log(data);
         }
     });
