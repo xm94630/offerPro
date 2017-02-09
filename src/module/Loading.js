10262a68e20428b97ceea7982491d2cfaefd237d
xm94630 <xm94630@126.com>
2017-02-09 21:56:54 +0800

    将mask层和loading层分开了~

--- src/module/Loading.js
+++ src/module/Loading.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import loading from '../img/triangle.gif'
+import Mask from './Mask.js'
 
 export default React.createClass({
   render() {
   	if(this.props.isShow) {
   	    return (       
   	    	<div className="loadingBox">
-            	<div className="mask"></div>
+            	<Mask />
             	<img src={loading}/>
         	</div>
         )
